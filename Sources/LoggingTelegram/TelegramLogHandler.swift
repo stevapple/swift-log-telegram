ea3660cde76be91619bd91dc6140f277cbc6048c
Wilson Yu <5325955+Xkinler@users.noreply.github.com>
2020-08-02 18:05:13 +0800

    Fix a typo and delete an unnecessary additional space

--- Sources/LoggingTelegram/TelegramLogHandler.swift
+++ Sources/LoggingTelegram/TelegramLogHandler.swift
@@ -61,10 +61,10 @@ public class TelegramLogHandler<T>: LogHandler where T: TelegramId {
   /// Creates a `TelegramLogHandler` for sending `Logger` output directly to Telegram.
   /// - Parameters:
   ///   - label: The log label for the log handler.
-  ///   - chat: Target chats to send to.
+  ///   - chat: The target chat to send to.
   ///   - token: Telegram Bot Token.
   ///   - level: (Optional) The minimal log level for this logger.
-  ///   - mute: (Optional)  Sending this message to the user silently
+  ///   - mute: (Optional) Sending this message to the user silently
   public init(
     label: String,
     token: String,
