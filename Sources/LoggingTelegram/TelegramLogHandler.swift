b25960ad09df94bcea33db6ddd29c2fd0204f7b9
Yehor Chernenko <yehorchernenko@lyft.com>
2023-05-27 20:54:52 +0800

    Update timestamp

--- Sources/LoggingTelegram/TelegramLogHandler.swift
+++ Sources/LoggingTelegram/TelegramLogHandler.swift
@@ -20,15 +20,9 @@ public var telegramLogDefaultLevel: Logger.Level = .critical
 /// Forked from `SlackLogHandler`.
 public class TelegramLogHandler<T>: LogHandler where T: TelegramId {
   private var timestamp: String {
-    var buffer = [Int8](repeating: 0, count: 255)
-    var timestamp = time(nil)
-    let localTime = localtime(&timestamp)
-    strftime(&buffer, buffer.count, "%Y-%m-%dT%H:%M:%S%z", localTime)
-    return buffer.withUnsafeBufferPointer {
-      $0.withMemoryRebound(to: CChar.self) {
-        String(cString: $0.baseAddress!)
-      }
-    }
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yyyy-MM-dd HH:MM:SS"
+      return formatter.string(from: Date())
   }
 
   /// The log label for the log handler.
